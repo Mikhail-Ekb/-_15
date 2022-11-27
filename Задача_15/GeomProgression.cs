3c4a04b9e966643ce0d4b70d69d9f450e0346790
MIKHAIL <MIKHAIL@DESKTOP-6MU8OHN>
2022-11-27 23:43:08 +0500

    Добавьте файлы проекта.

---
+++ Задача_15/GeomProgression.cs
@@ -0,0 +1,63 @@
+﻿namespace Задача_15
+{
+    interface ISeries
+    {
+        void SetStart(int x);
+        int GetNext();
+        void Reset();
+    }
+    class ArithProgression : ISeries
+    {
+        int startValue;
+        int currentValue;
+        int step;
+        public int GetNext()
+        {
+            currentValue += step;
+            return currentValue;
+        }
+
+        public void Reset()
+        {
+            currentValue = startValue;
+        }
+
+        public void SetStart(int x)
+        {
+            startValue = x;
+            currentValue = startValue;
+        }
+        public void SetStep(int s)
+        {
+            step = s;
+        }
+    }
+    class GeomProgression : ISeries
+    {
+        int startValue;
+        int currentValue;
+        int step;
+
+        public int GetNext()
+        {
+            currentValue *= step;
+            return currentValue;
+        }
+
+        public void Reset()
+        {
+            currentValue = startValue;
+        }
+
+        public void SetStart(int x)
+        {
+            startValue = x;
+            currentValue = startValue;
+        }
+        public void SetStep(int s)
+        {
+            step = s;
+        }
+
+    }
+}
---
+++ Задача_15/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithProgression progression1 = new ArithProgression();
+            Console.WriteLine("--------------------------\nАрифметическая прогрессия");
+            progression1.SetStart(10);
+            progression1.SetStep(2);
+            Console.WriteLine(progression1.GetNext());
+            Console.WriteLine(progression1.GetNext());
+            Console.WriteLine(progression1.GetNext());
+            progression1.Reset();
+            Console.WriteLine(progression1.GetNext());
+            Console.WriteLine(progression1.GetNext());
+
+
+            GeomProgression progression2 = new GeomProgression();
+            Console.Write("--------------------------\nГеометрическая прогрессия\nВведите стартовое число:");
+            progression2.SetStart(Convert.ToInt32(Console.ReadLine()));
+            Console.Write("Введите шаг прогрессии: ");
+            progression2.SetStep(Convert.ToInt32(Console.ReadLine()));
+            Console.Write("Необходимое количество чисел: ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            while (i != 0)
+            {
+                Console.Write($"{progression2.GetNext()} ");
+                i -= 1;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
